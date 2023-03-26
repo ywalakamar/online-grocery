39efaac1090213f80a4a47fad6376c75731ce53c
ywalakamar <barxwells@gmail.com>
2023-03-26 18:09:53 +0300

    refactor(env): use env variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import createApp from "./app.js";
 import dbConn from "./database/connection.js";
+
+const port = process.env.PORT;
 
 const server = async () => {
   const app = express();
@@ -9,9 +12,7 @@ const server = async () => {
 
   await createApp(app);
 
-  app.listen(8000, () => {
-    console.log(`listening at port 8000`);
-  });
+  app.listen(port);
 };
 
 server();
